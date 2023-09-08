9a5be07ece729f21e21c313b0b9a6dc03dc798df
jaoxmb <joaovictorcgdossantos@gmail.com>
2023-09-07 22:11:12 -0300

    added archives

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 const form = document.querySelector("form");
-const b = document.querySelector("#b");
-const a = document.querySelector("#a");
-const successMsg = document.querySelector("#success-msg");
+const fullName = document.querySelector("#fullName");
+const errorFullName = document.querySelector("#fullname-error");
+let nameIsFull = false;
 
-let msgIsVisible = false; // armazena o status de visibilidade para não executar o comando mais de uma vez sem necessidade.
-function msgVisible(boolean) {
-  if (boolean) {
-    if (!msgIsVisible) {
-      msgIsVisible = true;
-      successMsg.classList.remove("invisible");
-    }
-  } else if (msgIsVisible) {
-    msgIsVisible = false;
-    successMsg.classList.add("invisible");
-  }
+function checkFullName (name) {
+  const splitName = name.split(" ");
+  return splitName.length >= 2;
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  
-  msgVisible(true);
-  successMsg.innerHTML = `Está correto, o valor de B - ${b.value} é maior doque A - ${a.value}`;
-})
 
-a.addEventListener("keyup", (e) => {
-    const valueA = parseFloat(e.target.value);
-    b.min = valueA + 1;
+  const accountNumber = document.querySelector("#accountNumber");
+  const amount = document.querySelector("#amount");
+  const description = document.querySelector("#description");
+  const successMsg = document.querySelector("#success-msg");
 
-    msgVisible(false);
-})
+  if (nameIsFull) {
+    successMsg.classList.remove("invisible");
+    successMsg.innerHTML = `Transferencia realizada com sucesso para ${fullName.value}, conta ${accountNumber.value}. No valor de ${amount.value}. Descricao: ${description.value}`;
+  }
+});
 
-b.addEventListener("keyup", () => msgVisible(false));
+fullName.addEventListener("keyup", (e) => {
+  const el = e.target;
+  const checkCotainTagInvisible = errorFullName.classList.contains("invisible");
+  nameIsFull = checkFullName(el.value);
+
+  if (nameIsFull) {
+    if (!checkCotainTagInvisible) {
+      errorFullName.classList.add("invisible");
+      el.classList.remove("error");
+    }
+  } else if (checkCotainTagInvisible) {
+    errorFullName.classList.remove("invisible");
+    el.classList.add("error");
+  }
+});
